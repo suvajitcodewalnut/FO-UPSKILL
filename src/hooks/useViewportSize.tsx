c02c183f925d1837db243b0e005108e532f865f8
suvajitcodewalnut <suvajit@codewalnut.com>
2026-01-13 12:29:57 +0530

    feat: add custom-hook for viewport measurement

---
+++ src/hooks/useViewportSize.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+interface ViewportValues {
+	height: number;
+	width: number;
+}
+
+const getViewportSize = (): ViewportValues => {
+	if (typeof window === "undefined") {
+		return { width: 0, height: 0 };
+	}
+
+	return {
+		width: window.innerWidth,
+		height: window.innerHeight,
+	};
+};
+
+const useViewportSize = (): ViewportValues => {
+	const [viewport, setViewport] = useState<ViewportValues>(getViewportSize);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setViewport(getViewportSize());
+		};
+
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
+	return viewport;
+};
+
+export default useViewportSize;
