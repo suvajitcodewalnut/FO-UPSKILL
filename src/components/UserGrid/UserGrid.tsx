b4c47641f91701fbda66ccfbe68fc5fe3c86021a
suvajitcodewalnut <suvajit@codewalnut.com>
2026-01-19 16:49:01 +0530

    feat: add loader and error state

--- src/components/UserGrid/UserGrid.tsx
+++ src/components/UserGrid/UserGrid.tsx
@@ -11,11 +11,16 @@ import { GridThemeQuartz } from "../../constants/GridTheme";
 import CountryRenderer from "../Country/CountryRenderer";
 import useViewportSize from "../../hooks/useViewportSize";
 import { useQuery } from "@tanstack/react-query";
+import Loader from "../Loader/Loader";
 
 const UserGrid: React.FC = () => {
 	const { width } = useViewportSize();
 
-	const { data = [] } = useQuery<User[]>({
+	const {
+		data = [],
+		isLoading,
+		error,
+	} = useQuery<User[]>({
 		queryKey: ["userDataGridInformation"],
 		queryFn: () => axios.get("/users").then((response) => response.data),
 	});
@@ -54,7 +59,7 @@ const UserGrid: React.FC = () => {
 
 	if (width < 500) {
 		return (
-			<div className="h-screen w-full flex items-center justify-center bg-gray-700">
+			<div className="h-full w-full flex items-center justify-center bg-gray-700">
 				<div className="text-white font-bold">
 					WE ARE CURRENTLY ON DESKTOP !
 				</div>
@@ -62,10 +67,32 @@ const UserGrid: React.FC = () => {
 		);
 	}
 
+	if (isLoading) {
+		return (
+			<div className="h-full w-full flex items-center justify-center bg-gray-700">
+				<div className="text-white font-bold text-xl">
+					<Loader />
+				</div>
+			</div>
+		);
+	}
+
+	if (error) {
+		return (
+			<div className="h-full w-full flex items-center justify-center bg-gray-700">
+				<div className="text-red-400 font-bold text-xl">
+					Error loading data. Please try again.
+				</div>
+			</div>
+		);
+	}
+
 	return (
-		<div className="h-screen w-full flex flex-col items-center justify-center bg-gray-700">
-			<div className="mt-5 text-white font-bold text-xl">USER DATA GRID</div>
-			<div className="w-255 h-190 bg-white rounded-xl shadow-lg">
+		<div className="h-full w-full flex flex-col  bg-gray-700 p-4">
+			<div className="mb-4 text-white font-bold text-xl">
+				SIMPLE USER DATA GRID
+			</div>
+			<div className="flex-1 min-h-0 bg-white rounded-xl shadow-lg">
 				<AgGridReact
 					theme={GridThemeQuartz}
 					rowData={data}
